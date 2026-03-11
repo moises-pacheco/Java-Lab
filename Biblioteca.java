933503e0b08c4af87896cc245eeceb45cc1acbf8
moises-pacheco <zmoises.pacheco@gmail.com>
2026-03-11 23:31:08 +0100

    Refactorizando el metodo 'buscarLibro()'

--- Biblioteca.java
+++ Biblioteca.java
@@ -8,24 +8,23 @@ public class Biblioteca {
     private ArrayList<Libro> biblioteca; //"Base de datos" de nuestros libros
 
     //Constructor
-    public Biblioteca(){
+    public Biblioteca() {
         biblioteca = new ArrayList<>();
     }
 
     //Metodos
-    public void aniadirLibro(Libro l) {
-        biblioteca.add(l);
+    public void aniadirLibro(Libro libro) {
+        biblioteca.add(libro);
     }
 
     public void eliminarLibro() {
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Qué libro desea eliminar?");
         String nombreLibro = sc.nextLine();
-        //Busca por todos los libros hasta encontrar el buscado por el usuario.
-        //Eliminar libro:
-        for (Libro l : biblioteca) {
-            if (l.getTitulo().equalsIgnoreCase(nombreLibro)) {
-                biblioteca.remove(l);
+
+        for (Libro libro : biblioteca) {
+            if (libro.getTitulo().equalsIgnoreCase(nombreLibro)) {
+                biblioteca.remove(libro);
                 break;
             }
         }
@@ -33,48 +32,55 @@ public class Biblioteca {
 
     public void buscarLibro() {
         String respuestaUsuario;
-        do{
+        do {
             Scanner sc = new Scanner(System.in);
             System.out.println("¿Cómo quieres buscar el libro? \n1. Titulo \n2. Autor");
             respuestaUsuario = sc.nextLine();
             switch (respuestaUsuario) {
                 case "1":
-                    System.out.println("Indicanos el nombre del libro que deseas buscar:");
-                    String respuestaLibro = sc.nextLine();
-                    for (Libro l : biblioteca) {
-                        if (l.getTitulo().equalsIgnoreCase(respuestaLibro)) {
-                            System.out.println("El libro se encuentra en la base de datos.");
-                            l.mostrarInformacion();
-                            break;
-                        } else {
-                            System.out.println("El libro no se encuentra en la base de datos.");
-                            break;
-                        }
-                    }
+                    buscarLibroPorNombre(sc);
                     break;
-
                 case "2":
-                    System.out.println("Indicanos el nombre del autor del libro");
-                    String respuestaAutor = sc.nextLine();
-                    for(Libro l : biblioteca){
-                        //Mostrar todos los objetos que cumplan con esta condicion
-                        if(l.getAutor().equalsIgnoreCase(respuestaAutor)){
-                            //Si encuentra un libro, muestra ese libro.
-                            l.mostrarInformacion();
-                        }
-                    }
-                    break;  // Despues de terminar de recorrer el array, salir del bucle.
+                    buscarLibroPorAutor(sc);
+                    break;
                 default:
                     System.out.println("La eleccion no existe");
             }
-        }while(!respuestaUsuario.equalsIgnoreCase("1") && !respuestaUsuario.equalsIgnoreCase("2"));
+        } while (!respuestaUsuario.equalsIgnoreCase("1") && !respuestaUsuario.equalsIgnoreCase("2"));
+    }
 
+    private void buscarLibroPorAutor(Scanner sc) {
+        System.out.println("Indicanos el nombre del autor del libro");
+        String respuestaAutor = sc.nextLine();
+        for (Libro libro : biblioteca) {
+            //Mostrar todos los objetos que cumplan con esta condicion
+            if (libro.getAutor().equalsIgnoreCase(respuestaAutor)) {
+                //Si encuentra un libro, muestra ese libro.
+                libro.mostrarInformacion();
+            }
+        }
+    }
+
+    private void buscarLibroPorNombre(Scanner sc) {
+        System.out.println("Indicanos el nombre del libro que deseas buscar:");
+        String respuestaLibro = sc.nextLine();
+        boolean fueEncontrado = false;
+        for (Libro libro : biblioteca) {
+            if (libro.getTitulo().equalsIgnoreCase(respuestaLibro)) {
+                fueEncontrado = true;
+                libro.mostrarInformacion();
+                break;
+            }
+        }
+        if (!fueEncontrado) {
+            System.out.println("No se ha encontrado el libro en la base de datos.");
+        }
     }
 
     //Mostrar los libros que tenemos almacenados en nuestra biblioteca
     public void mostrarBiblioteca() {
         for (int i = 0; i < biblioteca.size(); i++) {
-            System.out.println("[" + (i+1) + "]: " + biblioteca.get(i).getTitulo());
+            System.out.println("[" + (i + 1) + "]: " + biblioteca.get(i).getTitulo());
         }
     }
 
--- Libro.java
+++ Libro.java
@@ -1,4 +1,5 @@
 package javaapplication1.PracticaBiblioteca;
+
 import java.time.LocalDateTime;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -14,93 +15,91 @@ public class Libro {
     private String titulo;
     private String autor;
     private LocalDateTime anioPublicacion;
-    private boolean disponibilidad; 
+    private boolean disponibilidad;
     private String personaLibro;
     public ArrayList<Libro> biblioteca = new ArrayList<>(); //Biblioteca donde tendremos todos nuestros libros
-    
+
     //Constructor:
-    public Libro(String titulo, String autor, LocalDateTime anioPublicacion, boolean disponibilidad){
+    public Libro(String titulo, String autor, LocalDateTime anioPublicacion, boolean disponibilidad) {
         this.titulo = titulo;
         this.autor = autor;
         this.anioPublicacion = anioPublicacion;
         this.disponibilidad = disponibilidad;
-        
+
     }
-    
+
     //Getters
-    public String getTitulo(){
+    public String getTitulo() {
         return titulo;
     }
-    
-    public String getAutor(){
+
+    public String getAutor() {
         return autor;
     }
-    
-    public LocalDateTime getAnioPublicacion(){
+
+    public LocalDateTime getAnioPublicacion() {
         return anioPublicacion;
     }
-    
-    public boolean getDisponibilidad(){
+
+    public boolean getDisponibilidad() {
         return disponibilidad;
     }
-    
+
     //Setters
-    public void setTitulo(String titulo){
+    public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    
-    public void setAutor(String autor){
+
+    public void setAutor(String autor) {
         this.autor = autor;
     }
-    
-    public void setAnioPublicacion(LocalDateTime anioPublicacion){
+
+    public void setAnioPublicacion(LocalDateTime anioPublicacion) {
         this.anioPublicacion = anioPublicacion;
     }
-    
-    public void setDisponibilidad(boolean disponibilidad){
+
+    public void setDisponibilidad(boolean disponibilidad) {
         this.disponibilidad = disponibilidad;
     }
-    
+
     //Metodos
-    
-    public void prestarLibro(){
+
+    public void prestarLibro() {
         //Antes de prestarlo, comprobar si esta en el sistema
         Scanner sc = new Scanner(System.in);
-        if(disponibilidad){
+        if (disponibilidad) {
             disponibilidad = false;
             System.out.println("¿A quién le prestarás el libro: " + titulo + "?");
             personaLibro = sc.nextLine();
             System.out.println("Has prestado el libro a: " + personaLibro);
-        }else{
+        } else {
             System.out.println("El libro ya ha sido prestado a la persona llamada: " + personaLibro);
         }
-        
-        
     }
-    
-    public void devolverLibro(){
+
+    public void devolverLibro() {
         //Si no está en el sistema, entonces permite devolverlo
-        if(!disponibilidad){
+        if (!disponibilidad) {
             disponibilidad = true;
-            System.out.println("Gracias por devolver el libro" );
-        }else{
+            System.out.println("Gracias por devolver el libro");
+        } else {
             System.out.println("Ya tenemos este libro, no hace falta.");
         }
     }
-    
-    public void mostrarInformacion(){
+
+    public void mostrarInformacion() {
         //Si está en el sistema, se muestra la información del libro
-        if(disponibilidad){
-            System.out.println("\nLibro: " + titulo + 
-            "\nAutor: " + autor + 
-            "\nAnio de publicacion: " + anioPublicacion + 
-            "\nDisponibiliad:" + disponibilidad); 
-        }else{
+        if (disponibilidad) {
+            System.out.println("\nLibro: " + titulo +
+                    "\nAutor: " + autor +
+                    "\nAnio de publicacion: " + anioPublicacion +
+                    "\nDisponibiliad:" + disponibilidad);
+        } else {
             //Si no está en el sistema, se muestra los datos del libro más el usuario quién lo tiene
-            System.out.println("\nLibro: " + titulo + 
-                    "\nAutor: " + autor + 
-                    "\nAnio de publicacion: " + anioPublicacion + 
-                    "\nDisponibilidad: " + disponibilidad + 
+            System.out.println("\nLibro: " + titulo +
+                    "\nAutor: " + autor +
+                    "\nAnio de publicacion: " + anioPublicacion +
+                    "\nDisponibilidad: " + disponibilidad +
                     "\nLo tiene: " + personaLibro);
         }
     }
